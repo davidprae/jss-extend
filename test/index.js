908e35684ebfa004fff0b4a55836512261f605f7
Oleg Slobodskoi <oleg008@gmail.com>
2016-09-06 16:58:58 +0200

    update tests to match jss@5.5.0

--- test/index.js
+++ test/index.js
@@ -13,64 +13,64 @@ QUnit.module('Extend plugin', {
 
 test('simple extend', function () {
   var a = {float: 'left'}
-  var ss = jss.createStyleSheet({
+  var sheet = jss.createStyleSheet({
     a: a,
     b: {
       extend: a,
       width: '1px'
     }
   }, {named: false})
-  ok(ss.rules.a)
-  ok(ss.rules.b)
-  equal(ss.toString(), 'a {\n  float: left;\n}\nb {\n  float: left;\n  width: 1px;\n}')
+  ok(sheet.getRule('a'))
+  ok(sheet.getRule('b'))
+  equal(sheet.toString(), 'a {\n  float: left;\n}\nb {\n  float: left;\n  width: 1px;\n}')
 })
 
 test('multi extend', function () {
   var a = {float: 'left'}
   var b = {position: 'absolute'}
-  var ss = jss.createStyleSheet({
+  var sheet = jss.createStyleSheet({
     c: {
       extend: [a, b],
       width: '1px'
     }
   }, {named: false})
-  ok(ss.rules.c)
-  equal(ss.toString(), 'c {\n  float: left;\n  position: absolute;\n  width: 1px;\n}')
+  ok(sheet.getRule('c'))
+  equal(sheet.toString(), 'c {\n  float: left;\n  position: absolute;\n  width: 1px;\n}')
 })
 
 test('nested extend', function () {
   var c = {float: 'left'}
   var b = {extend: c, display: 'none'}
-  var ss = jss.createStyleSheet({
+  var sheet = jss.createStyleSheet({
     a: {
       extend: b,
       width: '1px'
     }
   }, {named: false})
-  ok(ss.rules.a)
-  equal(ss.toString(), 'a {\n  float: left;\n  display: none;\n  width: 1px;\n}')
+  ok(sheet.getRule('a'))
+  equal(sheet.toString(), 'a {\n  float: left;\n  display: none;\n  width: 1px;\n}')
 })
 
 test('extend using rule name', function () {
-  var ss = jss.createStyleSheet({
+  var sheet = jss.createStyleSheet({
     a: {float: 'left'},
     b: {
       extend: 'a',
       width: '1px'
     }
   }, {named: false})
-  ok(ss.rules.a)
-  ok(ss.rules.b)
-  equal(ss.toString(), 'a {\n  float: left;\n}\nb {\n  float: left;\n  width: 1px;\n}')
+  ok(sheet.getRule('a'))
+  ok(sheet.getRule('b'))
+  equal(sheet.toString(), 'a {\n  float: left;\n}\nb {\n  float: left;\n  width: 1px;\n}')
 })
 
 test('error if extend using same rule name', function () {
-  var ss = jss.createStyleSheet({
+  var sheet = jss.createStyleSheet({
     a: {
       extend: 'a',
       width: '1px'
     }
   }, {named: false})
-  ok(ss.rules.a)
-  equal(ss.toString(), 'a {\n  width: 1px;\n}')
+  ok(sheet.getRule('a'))
+  equal(sheet.toString(), 'a {\n  width: 1px;\n}')
 })
