83da16f6e16ca711a1e3760a9b7d93556be1b596
Oleg Slobodskoi <oleg008@gmail.com>
2018-01-31 13:00:36 +0100

    early returns

--- src/index.js
+++ src/index.js
@@ -1,56 +1,68 @@
 import warning from 'warning'
 
 const isObject = obj => obj && typeof obj === 'object' && !Array.isArray(obj)
-const now = Date.now()
-const baseStyleNs = `baseStyle${now}`
+const baseStyleNs = `baseStyle${Date.now()}`
+
+function mergeExtend(style, rule, sheet, newStyle) {
+  // Extend using a rule name.
+  if (typeof style.extend === 'string') {
+    if (!sheet) return
+    const refRule = sheet.getRule(style.extend)
+    if (!refRule) return
+    if (refRule === rule) {
+      warning(false, '[JSS] A rule tries to extend itself \r\n%s', rule)
+      return
+    }
+    if (refRule.options.parent) {
+      const originalStyle = refRule.options.parent.rules.raw[style.extend]
+      extend(originalStyle, rule, sheet, newStyle)
+    }
+    return
+  }
+
+  // Extend using an array of objects.
+  if (Array.isArray(style.extend)) {
+    for (let index = 0; index < style.extend.length; index++) {
+      extend(style.extend[index], rule, sheet, newStyle)
+    }
+    return
+  }
+
+  // Extend is a style object.
+  for (const prop in style.extend) {
+    if (prop === 'extend') {
+      extend(style.extend.extend, rule, sheet, newStyle)
+      continue
+    }
+    if (isObject(style.extend[prop])) {
+      if (!(prop in newStyle)) newStyle[prop] = {}
+      extend(style.extend[prop], rule, sheet, newStyle[prop])
+      continue
+    }
+    newStyle[prop] = style.extend[prop]
+  }
+}
 
 /**
  * Recursively extend styles.
  */
 function extend(style, rule, sheet, newStyle = {}) {
-  if (typeof style.extend === 'string') {
-    if (sheet) {
-      const refRule = sheet.getRule(style.extend)
-      if (refRule) {
-        if (refRule === rule) warning(false, '[JSS] A rule tries to extend itself \r\n%s', rule)
-        else if (refRule.options.parent) {
-          const originalStyle = refRule.options.parent.rules.raw[style.extend]
-          extend(originalStyle, rule, sheet, newStyle)
-        }
-      }
-    }
-  }
-  else if (Array.isArray(style.extend)) {
-    for (let index = 0; index < style.extend.length; index++) {
-      extend(style.extend[index], rule, sheet, newStyle)
-    }
-  }
-  else {
-    for (const prop in style.extend) {
-      if (prop === 'extend') {
-        extend(style.extend.extend, rule, sheet, newStyle)
-      }
-      else if (isObject(style.extend[prop])) {
-        if (!newStyle[prop]) newStyle[prop] = {}
-        extend(style.extend[prop], rule, sheet, newStyle[prop])
-      }
-      else {
-        newStyle[prop] = style.extend[prop]
-      }
-    }
-  }
+  mergeExtend(style, rule, sheet, newStyle)
+
   // Copy base style.
   for (const prop in style) {
     if (prop === 'extend') continue
     if (isObject(newStyle[prop]) && isObject(style[prop])) {
       extend(style[prop], rule, sheet, newStyle[prop])
+      continue
     }
-    else if (isObject(style[prop])) {
+
+    if (isObject(style[prop])) {
       newStyle[prop] = extend(style[prop], rule, sheet)
+      continue
     }
-    else {
-      newStyle[prop] = style[prop]
-    }
+
+    newStyle[prop] = style[prop]
   }
 
   return newStyle
@@ -74,12 +86,13 @@ function copyBase(style, newStyle = {}) {
  */
 export default function jssExtend() {
   function onProcessStyle(style, rule, sheet) {
-    return style.extend ? extend(style, rule, sheet) : style
+    return 'extend' in style ? extend(style, rule, sheet) : style
   }
 
-  function onUpdate(data, rule) {
-    const {style, sheet} = rule
-    if (!style || (style.extend == null && rule[baseStyleNs] == null)) return
+  function onUpdate(data, rule, sheet) {
+    if (!('style' in rule)) return
+
+    const {style} = rule
 
     if (rule[baseStyleNs] == null) {
       rule[baseStyleNs] = copyBase(style)
